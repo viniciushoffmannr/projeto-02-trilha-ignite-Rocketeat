9c3df58d32b8f0b875870d34802c0584aeb567c0
Vinícius Hoffmann <vinicius.hoffram@hotmail.com>
2023-02-26 22:12:18 -0300

    adicionando arquivo de estilização global

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { ThemeProvider } from 'styled-components'
 import { Button } from './components/Button'
+import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
 export function App() {
@@ -10,6 +11,8 @@ export function App() {
       <Button variant="secondary" />
       <Button variant="danger" />
       <Button variant="success" />
+
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
