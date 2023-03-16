add80ec12a276745677a46708d3a934ab4f900a5
Vinícius Hoffmann <vinicius.hoffram@hotmail.com>
2023-03-15 21:56:41 -0300

    Criando rotas e inicicando layout definitivo

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
-import { useState } from 'react'
 import { ThemeProvider } from 'styled-components'
-import { Button } from './components/Button'
+import { Router } from './Router'
+import { BrowserRouter } from 'react-router-dom'
+
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="danger" />
-      <Button variant="success" />
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
